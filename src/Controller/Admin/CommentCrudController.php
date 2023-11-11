02b4d9f9ed80ba6e27742e650a1d532692a244b2
Alber <alberto.r.caballero.87@gmail.com>
2023-11-11 11:47:47 +0100

    customize admin panel for comments

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -7,6 +7,10 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 
 class CommentCrudController extends AbstractCrudController
 {
@@ -15,14 +19,34 @@ class CommentCrudController extends AbstractCrudController
         return Comment::class;
     }
 
-    /*
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->disable(Crud::PAGE_DETAIL, Action::NEW)
+            ->disable(Crud::PAGE_DETAIL, Action::EDIT)
+        ;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Comment')
+            ->setEntityLabelInPlural('Comments')
+            ->setSearchFields(['user.username', 'user.email', 'content'])
+            ->setDefaultSort(['created_at' => 'DESC'])
+        ;
+    }
+
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('user.username', 'User'),
+            TextField::new('post.title', 'Post'),
+            DateTimeField::new('created_at', 'Created At'),
+            TextEditorField::new('content'),
         ];
     }
-    */
+
 }
