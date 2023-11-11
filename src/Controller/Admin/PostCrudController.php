22702c327958922f1831b23f0ba04ed013b3b286
Alber <alberto.r.caballero.87@gmail.com>
2023-11-11 16:35:43 +0100

    add improvents for admin panel

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -3,10 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 
 class PostCrudController extends AbstractCrudController
 {
@@ -15,14 +19,44 @@ class PostCrudController extends AbstractCrudController
         return Post::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Post')
+            ->setEntityLabelInPlural('Posts')
+            ->setSearchFields(['user.username', 'user.email', 'tag.name'])
+            ->setDefaultSort(['created_at' => 'DESC', 'publication_date' => 'DESC'])
+        ;
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('user.username', 'User')->hideOnForm(),
+            TextField::new('title', 'Title'),
+            TextField::new('slug')->onlyOnForms(),
+            DateTimeField::new('updated_at')->hideOnForm(),
+            DateTimeField::new('created_at')->hideOnForm(),
+            DateTimeField::new('publication_date'),
+            BooleanField::new('is_draft'),
+            TextEditorField::new('content')->onlyOnForms(),
         ];
     }
-    */
+
+    public function createEntity(string $entityFqcn)
+    {
+        $post = new Post();
+        $post->setUser($this->getUser());
+        $post->setCreatedAt(new \DateTimeImmutable());
+
+        return $post;
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $entityInstance->setUpdatedAt(new \DateTimeImmutable());
+        $entityManager->persist($entityInstance);
+        $entityManager->flush();
+    }
 }
